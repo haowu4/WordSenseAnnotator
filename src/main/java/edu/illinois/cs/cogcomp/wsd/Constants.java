384485e5d5b36a5a71700fe7a8a9b3c554138a04
Hao Wu <haowu@haowu.me>
2017-01-15 18:56:40 -0600

    first commit

---
+++ src/main/java/edu/illinois/cs/cogcomp/wsd/Constants.java
@@ -0,0 +1,8 @@
+package edu.illinois.cs.cogcomp.wsd;
+
+/**
+ * Created by haowu4 on 1/14/17.
+ */
+public class Constants {
+    public static final String VIEWNAME = "SENSE";
+}
---
+++ src/test/java/edu/illinois/cs/cogcomp/wsd/PrintResult.java
@@ -0,0 +1,25 @@
+package edu.illinois.cs.cogcomp.wsd;
+
+import edu.illinois.cs.cogcomp.annotation.AnnotatorException;
+import edu.illinois.cs.cogcomp.annotation.AnnotatorServiceConfigurator;
+import edu.illinois.cs.cogcomp.annotation.BasicAnnotatorService;
+import edu.illinois.cs.cogcomp.core.datastructures.textannotation
+        .TextAnnotation;
+import edu.illinois.cs.cogcomp.core.utilities.configuration.Configurator;
+import edu.illinois.cs.cogcomp.core.utilities.configuration.Property;
+import edu.illinois.cs.cogcomp.core.utilities.configuration.ResourceManager;
+import edu.illinois.cs.cogcomp.pipeline.common.PipelineConfigurator;
+import edu.illinois.cs.cogcomp.pipeline.main.PipelineFactory;
+import edu.illinois.cs.cogcomp.wsd.annotators.WordSenseAnnotator;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import static edu.illinois.cs.cogcomp.wsd.Constants.VIEWNAME;
+
+/**
+ * Created by haowu4 on 1/14/17.
+ */
+public class PrintResult {
+
+}
